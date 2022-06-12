cb5c6a8fd9e8c112c169ce9bc7514728197d528d
SafiaMaani <saf.maani@gmail.com>
2022-06-12 21:30:52 +0100

    Modification sur le style des pages Categorie

--- Views/Parfums.php
+++ Views/Parfums.php
@@ -11,9 +11,9 @@
         foreach ($produits as $produit) {
         ?>
             <div class="cards w-25 p-2" style="height: 300px;">
-                <div class="" style="height: 100%;">
+                <div class="text-center" style="height: 100%;">
                     <img src="Views/assets/img/boutique/<?= $produit['categorie'] ?>/<?= $produit['img'] ?>" alt="huile" style="width:100%; height : 80%;">
-                    <h5 class="m-1" style="height: 5%;"><?= $produit['name'] ?></h5>
+                    <h5><?= $produit['name'] ?></h5>
                     <button class="btn btn-dark" style="height: 13%;">Ajouter au panier</button>
                 </div>
             </div>
--- Views/boutique.php
+++ Views/boutique.php
@@ -13,7 +13,7 @@
             <div class="cards w-25 p-3" style="height: 300px;">
                 <div class="text-center" style="height: 100%;">
                     <img src="Views/assets/img/boutique/<?= $produit['categorie'] ?>/<?= $produit['img'] ?>" alt="huile" style="width:100%; height : 80%;">
-                    <h5 style="width:80%; height: 5%;"><?= $produit['name'] ?></h5>
+                    <h5><?= $produit['name'] ?></h5>
                     <button class="btn btn-dark" style="height: 13%;">Ajouter au panier</button>
                 </div>
             </div>
